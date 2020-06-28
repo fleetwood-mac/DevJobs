dfa0ac6b13569044b51e77f018b53081a30436ad
caciqu3s <richardmarques016@gmail.com>
2020-06-27 21:16:50 -0300

    creaing job model

---
+++ src/model/job.ts
@@ -0,0 +1,13 @@
+export class Job {
+    id!: string;
+    type!: string;
+    url!: string;
+    created_at!: Date; 
+    company!: string;
+    company_url!: string;
+    location!: string;
+    title!: string;
+    description!: string;
+    how_to_apply!: string;
+    company_logo!: string;
+}
---
+++ src/pages/jobs/jobDetails/jobDetails.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Job } from '../../../model/job';
+
+// import { Container } from './styles';
+
+type JobDetailProps = {
+    job: Job
+}
+
+const JobDetails: React.FC = (props) => {
+  return <View />;
+}
+
+export default jobDetails;
