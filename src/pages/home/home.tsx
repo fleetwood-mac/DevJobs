33709b604ac4648343f43fc64be98415afc82b84
Gabriel <creeplays@gmail.com>
2020-06-28 01:12:05 -0300

    Prototipo de pagina inicial.

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
+import { useFonts, Roboto_900Black } from '@expo-google-fonts/roboto';
+
 import { View, Text, SafeAreaView, Button } from "react-native";
 
 import style from './style'
@@ -10,21 +12,44 @@ let headerConfig : ParamsHeader =
 {
     params:
     {
-        hasBackButton:true,
-        hasImage:false,
+        hasBackButton:false,
+        hasImage:true,
         hasLogo:true
     }
 }
 
 export default function Home()
 {
+    let [fontsLoaded] = useFonts({
+        Roboto_900Black,
+      });
 
+      if(!fontsLoaded)
+      {
+          return(
+            <SafeAreaView style={style.container}>
+                <Text>Carregando</Text>
+            </SafeAreaView>
+          )
+      }
+      else
+      {
+        return(
+            <SafeAreaView style={style.container}>
+                <View style={style.view}>
+                    <Header params={headerConfig.params}/>
 
-    return(
-        <SafeAreaView style={style.container}>
-            <View style={style.view}>
-                <Header params={headerConfig.params}/>
-            </View>
-        </SafeAreaView>
-    )
+                    <View style={{paddingTop:25}}>
+                        <Text style={{marginBottom:2}}>Bem vindo!</Text>
+                        <Text style={{fontFamily:'Roboto_900Black',fontSize:30,flexWrap:"wrap",marginBottom:30}}>O que quer encontrar de novo hoje?</Text>
+                        <View style={{height:50,backgroundColor:"green",borderRadius:10}}>
+
+                        </View>
+                    
+                    </View>
+                </View>
+            </SafeAreaView>
+        );
+      }
+        
 }
