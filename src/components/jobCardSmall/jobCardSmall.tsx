3b16bb576d2626d2dfc77c44489bfabb5ce609a1
Gabriel <creeplays@gmail.com>
2020-08-15 11:57:51 -0300

    feat(component): cards de job em tamanho pequeno e alguns ajustes pra montar highlights de tela

--- src/components/jobCardSmall/jobCardSmall.tsx
+++ src/components/jobCardSmall/jobCardSmall.tsx
@@ -1,17 +1,20 @@
 import { View, Image, Text } from "react-native"
 import React from 'react';
+import { Job } from "../../model/job";
 
+type JobCardProps = {
+    job: Job
+}
 
-
-const JobCardSmall = (props: any) =>
+const JobCardSmall = ({ job }: JobCardProps) =>
 {
-    let {image,description,company} = props;
+    let {description,company} = job;
     return(
         <View style={{width:'100%',height:50,backgroundColor:"white",borderRadius:10,padding:12,paddingLeft:14,display:'flex',flexDirection:'column',marginBottom:10}}>
       
             <View style={{display:'flex',justifyContent:'flex-end',flex:1}}>
                 <View style={{height:30,display:'flex',flexDirection:'row',marginLeft:-5}}>
-                    <Image source={image} style={{width:35,height:35,marginRight:10}}/>
+                    {/* <Image source={image} style={{width:35,height:35,marginRight:10}}/> */}
                     <View style={{display:'flex',flexDirection:'column',paddingVertical:4}}>
                         <Text style={{color:"black", fontFamily:'Roboto_500Medium',fontSize:12, marginBottom:1}}>{description}</Text>
                         <Text style={{color:"black", fontFamily:'Roboto_100Thin',fontSize:10}}>{company}</Text>
